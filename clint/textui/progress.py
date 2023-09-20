ebdd3031fb508532b6811983c7cec689065ebee7
Helvio Junior <helvio_junior@hotmail.com>
2023-09-20 15:53:31 -0300

    Progressbar features

--- clint/textui/progress.py
+++ clint/textui/progress.py
@@ -10,17 +10,24 @@ This module provides the progressbar functionality.
 
 from __future__ import absolute_import
 
+import re
 import sys
 import time
 
 STREAM = sys.stderr
 
-BAR_TEMPLATE = '%s[%s%s] %i/%i - %s\r'
+BAR_TEMPLATE = ('\033[0m \033[32m{label}\033[0m |{filled_chars}\033[38;5;240m{empty_chars}\033[0m| '
+                '\033[32m{percent}\033[33m{progress}/{expected_text}{unit_label}\033[0m | '
+                '\033[38;5;243m{eta}\033[0m     \r'
+                '\033[0m \033[32m{label}\033[0m |{filled_chars}')
 MILL_TEMPLATE = '%s %s %i/%i\r'
+TIME_FMT_H = 'ETA: %Hh%Mm'
+TIME_FMT_M = 'ETA: %Mm%Ss'
+TIME_FMT_S = 'ETA: %Ss'
 
 DOTS_CHAR = '.'
-BAR_FILLED_CHAR = '#'
-BAR_EMPTY_CHAR = ' '
+BAR_FILLED_CHAR = '█'
+BAR_EMPTY_CHAR = '░'
 MILL_CHARS = ['|', '/', '-', '\\']
 
 # How long to wait before recalculating the ETA
@@ -39,7 +46,8 @@ class Bar(object):
         return False  # we're not suppressing exceptions
 
     def __init__(self, label='', width=32, hide=None, empty_char=BAR_EMPTY_CHAR,
-                 filled_char=BAR_FILLED_CHAR, expected_size=None, every=1):
+                 filled_char=BAR_FILLED_CHAR, expected_size=None, every=1,
+                 show_percent=False, unit='', unit_label=None, custom_format=None, disable_color=False):
         self.label = label
         self.width = width
         self.hide = hide
@@ -49,58 +57,168 @@ class Bar(object):
                 self.hide = not STREAM.isatty()
             except AttributeError:  # output does not support isatty()
                 self.hide = True
-        self.empty_char =    empty_char
-        self.filled_char =   filled_char
+        self.empty_char = ' ' if disable_color and empty_char == BAR_EMPTY_CHAR else empty_char
+        self.filled_char = '#' if disable_color and filled_char == BAR_FILLED_CHAR else filled_char
         self.expected_size = expected_size
-        self.every =         every
-        self.start =         time.time()
-        self.ittimes =       []
-        self.eta =           0
-        self.etadelta =      time.time()
-        self.etadisp =       self.format_time(self.eta)
+        self.expected_text = f'{expected_size:.0f}'
+        self.every = every
+        self.start = time.time()
+        self.ittimes = []
+        self.eta = 0
+        self.elapsed = 0
+        self.etadelta = time.time()
+        self.etadisp = self.format_time(self.eta)
         self.last_progress = 0
-        if (self.expected_size):
+        self.show_percent = show_percent
+        self.template = custom_format if custom_format else BAR_TEMPLATE
+        self.unit = unit
+        self.unit_label = f' {unit_label}' if unit_label is not None and unit_label.strip() != '' else ''
+        self.last_text_bar = ''
+
+        if disable_color:
+            self.template = self.escape_ansi(self.template)
+
+        self.template = '\r' + self.template.strip('\r')
+
+        if self.expected_size:
+            self.calculate_expected()
             self.show(0)
 
     def show(self, progress, count=None):
         if count is not None:
             self.expected_size = count
+            self.calculate_expected()
         if self.expected_size is None:
             raise Exception("expected_size not initialized")
         self.last_progress = progress
+        if progress > self.expected_size:
+            self.expected_size = progress
         if (time.time() - self.etadelta) > ETA_INTERVAL:
             self.etadelta = time.time()
             self.ittimes = \
                 self.ittimes[-ETA_SMA_WINDOW:] + \
-                    [-(self.start - time.time()) / (progress+1)]
+                [-(self.start - time.time()) / (progress + 1)]
             self.eta = \
                 sum(self.ittimes) / float(len(self.ittimes)) * \
                 (self.expected_size - progress)
             self.etadisp = self.format_time(self.eta)
         x = int(self.width * progress / self.expected_size)
         if not self.hide:
-            if ((progress % self.every) == 0 or      # True every "every" updates
-                (progress == self.expected_size)):   # And when we're done
-                STREAM.write(BAR_TEMPLATE % (
-                    self.label, self.filled_char * x,
-                    self.empty_char * (self.width - x), progress,
-                    self.expected_size, self.etadisp))
-                STREAM.flush()
+            if ((progress % self.every) == 0 or  # True every "every" updates
+                    (progress == self.expected_size)):  # And when we're done
+
+                txt = self.get_text_bar(progress)
+                if txt != self.last_text_bar:  # write only when we have changes in text
+                    self.last_text_bar = txt
+                    self.write_output(txt)
+
+    def get_text_bar(self, progress):
+        if not self.hide:
+            x = int(self.width * progress / self.expected_size)
+            return self.template.format(
+                label=self.label,
+                filled_chars=self.filled_char * x,
+                empty_chars=self.empty_char * (self.width - x),
+                progress=self.format_unit(progress, self.unit),
+                expected_text=self.expected_text,
+                eta=self.etadisp,
+                percent=self.format_percent(progress),
+                unit_label=self.unit_label
+            )
+        return ''
 
     def done(self):
         self.elapsed = time.time() - self.start
-        elapsed_disp = self.format_time(self.elapsed)
         if not self.hide:
             # Print completed bar with elapsed time
-            STREAM.write(BAR_TEMPLATE % (
-                self.label, self.filled_char * self.width,
-                self.empty_char * 0, self.last_progress,
-                self.expected_size, elapsed_disp))
-            STREAM.write('\n')
-            STREAM.flush()
+            x = int(self.width * self.last_progress / self.expected_size)
+            self.write_output(
+                self.template.format(
+                    label=self.label,
+                    filled_chars=self.filled_char * x,
+                    empty_chars=self.empty_char * (self.width - x),
+                    progress=self.format_unit(self.last_progress, self.unit),
+                    expected_text=self.expected_text,
+                    eta=self.format_time(self.elapsed),
+                    percent=self.format_percent(self.last_progress),
+                    unit_label=self.unit_label
+                ), True
+            )
 
-    def format_time(self, seconds):
-        return time.strftime('%H:%M:%S', time.gmtime(seconds))
+    def clear_line(self):
+        if not self.hide:
+            x = len(self.escape_ansi(self.get_text_bar(self.expected_size)))
+            self.write_output((' ' * (x + 4)) + '\r', new_line=False)
+
+    # Expose method to permit print a new line (in stdout) without trash chars at screen
+    def print_line(self, text=''):
+        self.clear_line()
+        print(text)
+        self.write_output(self.get_text_bar(self.last_progress))
+
+    def format_percent(self, progress):
+        if not self.show_percent:
+            return ''
+
+        p = (progress / self.expected_size) * 100
+        return f' {p:.2f}% '
+
+    @classmethod
+    def format_time(cls, seconds):
+        if seconds > 60 * 60:  # 1 hour
+            return time.strftime(TIME_FMT_H, time.gmtime(seconds))
+        elif seconds > 60:  # 1 minute
+            return time.strftime(TIME_FMT_M, time.gmtime(seconds))
+        else:
+            return time.strftime(TIME_FMT_S, time.gmtime(seconds))
+
+    @classmethod
+    def write_output(cls, text='', new_line=False):
+        STREAM.write(text)
+        if new_line:
+            STREAM.write('\n')
+        STREAM.flush()
+
+    @classmethod
+    def format_unit(cls, num=0, unit=''):
+        if unit.lower() in ["b", "kb", "mb", "gb", "tb", "pb", "ep", "zb"]:
+            return cls.format_bytes_unit(num, start_unit=unit.upper().replace('B', ''))
+        else:
+            return f"{num:.0f}"
+
+    def calculate_expected(self):
+        self.expected_text = self.format_unit(self.expected_size, self.unit)
+
+        num = self.expected_size
+        started = False
+        for unit in ["zb", "ep", "pb", "tb", "gb", "mb", "kb", "b"]:
+            if started:
+                num *= 1024.0
+                self.unit = unit
+
+            if self.unit.lower() == unit:
+                started = True
+
+        self.expected_size = int(num)
+
+    @classmethod
+    def format_bytes_unit(cls, num, suffix="B", start_unit=""):
+        started = False
+        for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+            if started or start_unit.upper() == unit:
+                started = True
+                if abs(num) < 1024.0:
+                    return f"{num:3.1f}{unit}{suffix}"
+                num /= 1024.0
+        return f"{num:.1f}{suffix}"
+
+    @classmethod
+    def escape_ansi(cls, text=None):
+        if text is None:
+            return ''
+
+        pattern = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+        return pattern.sub('', text)
 
 
 def bar(it, label='', width=32, hide=None, empty_char=BAR_EMPTY_CHAR,
@@ -127,7 +245,7 @@ def dots(it, label='', hide=None, every=1):
 
     for i, item in enumerate(it):
         if not hide:
-            if i % every == 0:         # True every "every" updates
+            if i % every == 0:  # True every "every" updates
                 STREAM.write(DOTS_CHAR)
                 sys.stderr.flush()
 
@@ -150,8 +268,8 @@ def mill(it, label='', hide=None, expected_size=None, every=1):
 
     def _show(_i):
         if not hide:
-            if ((_i % every) == 0 or         # True every "every" updates
-                (_i == count)):            # And when we're done
+            if ((_i % every) == 0 or  # True every "every" updates
+                    (_i == count)):  # And when we're done
 
                 STREAM.write(MILL_TEMPLATE % (
                     label, _mill_char(_i), _i, count))
--- examples/progressbar.py
+++ examples/progressbar.py
@@ -4,6 +4,8 @@
 import sys
 import os
 
+from clint.textui.colored import ColoredString
+
 sys.path.insert(0, os.path.abspath('..'))
 
 from time import sleep
@@ -14,13 +16,20 @@ from clint.textui import progress
 if __name__ == '__main__':
     for i in progress.bar(range(100)):
         sleep(random() * 0.2)
-        
+
+    with progress.Bar(label="Bytes formatted", expected_size=2, unit='Gb', unit_label='Size',
+                      show_percent=True, every=1024 * 10) as bar:
+        for val in range(0, 2 * 1024 * 1024 * 1024, 3 * 1024):
+            bar.show(val)
+
     with progress.Bar(label="nonlinear", expected_size=10) as bar:
         last_val = 0
         for val in (1,2,3,9,10):
             sleep(2 * (val - last_val))
             bar.show(val)
             last_val = val
+            if val == 3:
+                bar.print_line("Text line printed!")
 
     for i in progress.dots(range(100)):
         sleep(random() * 0.2)
