5cd13fcbf37b9f739844b5aa60d0aa56786fd954
Helvio Junior <helvio_junior@hotmail.com>
2023-09-20 18:25:26 -0300

    Bugfix at clear line

--- clint/textui/progress.py
+++ clint/textui/progress.py
@@ -210,8 +210,8 @@ class Bar(object):
 
         self.expected_size = int(num)
         self.line_size = max([
-            len(x) + 10 for x in self.escape_ansi(self.get_text_bar(self.expected_size)).replace('\n', '\r').split('\r')
-        ] + [self.line_size])
+            len(x) for x in self.escape_ansi(self.get_text_bar(self.expected_size)).replace('\n', '\r').split('\r')
+        ] + [self.line_size]) + 16
 
     @classmethod
     def format_bytes_unit(cls, num, suffix="B", start_unit=""):
